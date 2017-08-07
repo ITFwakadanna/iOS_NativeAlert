64cc7815cb1fef8ca9c283ef51da8bdfce693017
hoehoe <hoehoe@chocbanana.com>
2017-08-07 19:30:31 +0900

    commit.

---
+++ Xcode/NativeAlert/NativeAlert.h
@@ -0,0 +1,14 @@
+//
+//  NativeAlert.h
+//  NativeAlert
+//
+//  Created by hoehoe on 2012/10/06.
+//  Copyright (c) 2012年 hoehoe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Adobe AIR/Adobe AIR.h>
+
+@interface NativeAlert : NSObject<UIAlertViewDelegate>
+
+@end
